f8c57a7a56c99aca0fc68260c441961334c676df
Adam Craig and Ashley Maceli <adam@>
2016-06-27 14:04:33 -0700

    Add functionality for passing user inputs from Main Activity to the StoryView Activity, so that they can view the story made with their input

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -5,20 +5,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button mLibsSubmitButton;
+    private EditText mNoun1EditText;
+    private EditText mVerb1EditText;
+    private EditText mAdjective1EditText;
+    private EditText mProperNoun1EditText;
+    private EditText mAnimal1EditText;
+    private EditText mVerb2EditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNoun1EditText = (EditText) findViewById(R.id.noun1EditText);
+        mVerb1EditText = (EditText) findViewById(R.id.verb1EditText);
+        mAdjective1EditText = (EditText) findViewById(R.id.adjective1EditText);
+        mProperNoun1EditText = (EditText) findViewById(R.id.properNoun1EditText);
+        mAnimal1EditText = (EditText) findViewById(R.id.animal1EditText);
+        mVerb2EditText = (EditText) findViewById(R.id.verb2EditText);
 
         mLibsSubmitButton = (Button) findViewById(R.id.libsSubmitButton);
             mLibsSubmitButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    String noun1 = mNoun1EditText.getText().toString();
+                    String verb1 = mVerb1EditText.getText().toString();
+                    String adjective1 = mAdjective1EditText.getText().toString();
+                    String properNoun1 = mProperNoun1EditText.getText().toString();
+                    String animal1 = mAnimal1EditText.getText().toString();
+                    String verb2 = mVerb2EditText.getText().toString();
                     Intent intent = new Intent(MainActivity.this, StoryViewActivity.class);
+                    intent.putExtra("noun1", noun1);
+                    intent.putExtra("verb1", verb1);
+                    intent.putExtra("adjective1", adjective1);
+                    intent.putExtra("properNoun1", properNoun1);
+                    intent.putExtra("animal1", animal1);
+                    intent.putExtra("verb2", verb2);
+
                     startActivity(intent);
                 }
             });
--- app/src/main/java/com/epicodus/madlibs/StoryViewActivity.java
+++ app/src/main/java/com/epicodus/madlibs/StoryViewActivity.java
@@ -1,13 +1,26 @@
 package com.epicodus.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StoryViewActivity extends AppCompatActivity {
+
+    private TextView mStoryTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_view);
+        mStoryTextView = (TextView) findViewById(R.id.storyTextView);
+        Intent intent = getIntent();
+        String noun1 = intent.getStringExtra("noun1");
+        String verb1 = intent.getStringExtra("verb1");
+        String adjective1 = intent.getStringExtra("adjective1");
+        String properNoun1 = intent.getStringExtra("properNoun1");
+        String animal1 = intent.getStringExtra("animal1");
+        String verb2 = intent.getStringExtra("verb2");
+        mStoryTextView.setText("A long, long time ago, in the Kingdom of " + noun1 + ", there lived a stupid giant. His only desire in the world was to " + verb1 + " as many people as possible. These people must be " + adjective1 + " or he would refuse to " + verb1 + " them. Suddenly, a magnificent hero named " + properNoun1 + " appeared! He was accompanied by his miniature " + animal1 + ", Bub. " + properNoun1 + " and Bub made sure that the stupid giant never " + verb1 + " or " + verb2 + " again.");
     }
 }
