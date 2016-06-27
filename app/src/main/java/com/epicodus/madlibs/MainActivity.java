e32536ce08e517d7a768c6c76bb6d06746fa8989
Adam Craig and Ashley Maceli <ashley@>
2016-06-27 13:22:31 -0700

    add intent to MainActivity to transition to StoryViewActivity on button click

--- app/src/main/java/com/epicodus/madlibs/MainActivity.java
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -1,13 +1,26 @@
 package com.epicodus.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mLibsSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLibsSubmitButton = (Button) findViewById(R.id.libsSubmitButton);
+            mLibsSubmitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, StoryViewActivity.class);
+                    startActivity(intent);
+                }
+            });
     }
 }
