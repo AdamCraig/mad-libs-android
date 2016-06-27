a450912a8938a94971228f8b59035ea2faf963f5
Adam Craig and Ashley Maceli <adam@>
2016-06-27 15:29:46 -0700

    Add passing instrumentation test that checks for the first inputted Noun being displayed in the StoryView Activity

--- app/src/androidTest/java/com/epicodus/madlibs/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/epicodus/madlibs/MainActivityInstrumentationTest.java
@@ -6,6 +6,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -21,4 +22,15 @@ public class MainActivityInstrumentationTest {
         onView(withId(R.id.noun1EditText)).perform(typeText("Spaghetti"))
                 .check(matches(withText("Spaghetti")));
     }
+
+    @Test
+    public void noun1IsSentToStoryActivity() {
+        String noun1 = "Dinosaur";
+        String story = "A long, long time ago, in the Kingdom of Dinosaur, there lived a stupid giant. His only desire in the world was to  as many people as possible. These people must be  or he would refuse to  them. Suddenly, a magnificent hero named  appeared! He was accompanied by his miniature , Bub.  and Bub made sure that the stupid giant never  or  again.";
+        onView(withId(R.id.noun1EditText)).perform(typeText(noun1));
+        onView(withId(R.id.libsSubmitButton)).perform(click());
+        onView(withId(R.id.storyTextView)).check(matches(withText(story)));
+    }
+
+
 }
